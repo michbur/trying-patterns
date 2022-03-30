d56ed66b967632e11aa2f7dd5887edcce3dcb5e9
michbur <michalburdukiewicz@gmail.com>
2022-03-30 23:08:06 +0200

    finished

--- pattern-plot.R
+++ pattern-plot.R
@@ -29,7 +29,6 @@ col_df <- expand.grid(unique(df[["type"]]), unique(df[["type"]])) %>%
   mutate(name = paste0(unique(c(Var1, Var2)), collapse = "")) %>% 
   mutate(col1 = base_colors[Var1], col2 = base_colors[Var2])
 
-col_vec <- col_df
 
 hm_df <- lapply(unique(df[["type"]]), function(ith_type) {
   min_pos <- min(df[df[["type"]] == ith_type, "start"])
@@ -42,10 +41,12 @@ hm_df <- lapply(unique(df[["type"]]), function(ith_type) {
   pivot_longer(cols = -aa_pos) %>% 
   mutate(type_name = ifelse(value, name, "")) %>% 
   group_by(aa_pos) %>% 
-  summarise(type = paste0(type_name, collapse = ""))
+  summarise(type = paste0(type_name, collapse = "")) %>% 
+  inner_join(col_df, by = c("type" = "name"))
 
 ggplot(hm_df, aes(xmin = aa_pos, xmax = aa_pos + 1, 
                ymin = -0.25, ymax = 0.25, 
-               fill = type)) +
-  geom_rect_pattern(color = "black") + 
-  scale_fill_manual(values = c("#D5E8D4", "#F8CECC"))
+               pattern_fill  = I(col1),
+               pattern_fill2 = I(col2),)) +
+  geom_rect_pattern(color = "black", pattern = "gradient") +
+  scale_y_continuous(limits = c(-5, 5))
